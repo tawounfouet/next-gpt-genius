59f9a36790787f84fd776f951c00d25ac0e87068
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2023-12-27 04:14:41 +0100

    setting openAi api and display chatmessages

--- utils/action.js
+++ utils/action.js
@@ -1,6 +1,27 @@
 'use server'
+import OpenAI from "openai"
 
-export const generateChatResponse = async (chatMessage) => {
-    console.log(chatMessage)
-    return 'awesome'
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+export const generateChatResponse = async (chatMessages) => {
+    try {
+        const response = await openai.chat.completions.create({
+            messages : [
+                {
+                    role: 'system', content: "you are a helpful assistant",
+                    ...chatMessages
+                },
+            ],
+            model: "gpt-3.5-turbo",
+            temperature: 0,
+        })
+    } catch (error) {
+        // console.log(error)
+        return null;
+    }
+    // console.log(response.choices[0].message)
+    // console.log(response)
+    return response.choices[0].message;
 }
